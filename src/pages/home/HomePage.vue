87b5dc4539ee990fc1de221e95f5eda008f72112
sotaro maehara <s.maehara81@icloud.com>
2018-04-15 20:25:14 +0900

    add recommend card

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -3,8 +3,23 @@
   text-align: center;
 }
 .body {
-  margin-top: 100px;
+  margin-top: 50px;
 }
+.el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
 </style>
 
 <template>
@@ -16,10 +31,18 @@
       <img src="../../assets/love.jpeg" width="12" height="12">
       デートプランを探す
     </v-ons-list-header>
-        <v-ons-list-item v-for='region in regions' @click="$router.push({ name: region.url});">
+        <v-ons-list-item v-for='region in regions' :key='region' @click="$router.push({ name: region.url});">
           {{region.name}}
         </v-ons-list-item>
     </v-ons-list>
+    <h1>話題のデート体験記</h1>
+    <el-carousel height="250px">
+    <el-carousel-item v-for="item in experiences" :key="item">
+      <h3>{{item.title}}</h3>
+      {{item.detail}}
+      <p font-size="2px">続きを読む</p>
+    </el-carousel-item>
+    </el-carousel>  
     <v-ons-button modifier="cta" style="margin: 6px 0" @click="$router.push({ name: 'create-plan' });">作成</v-ons-button>
   </div>
   </v-ons-page>
@@ -48,6 +71,16 @@ export default {
         {
           name: 'プランで探す',
           url: 'home',
+        },
+      ],
+      experiences: [
+        {
+          title: 'えはまの奮発日記',
+          detail: 'tinderで知り合った女性と食事することになりました。しかし女性の右手には...',
+        },
+        {
+          title: 'ほげ',
+          detail: 'hogehoge',
         },
       ],
     };
