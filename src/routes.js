d02724d3b1637e3132d7f20c82f7fb99df13a4e5
Eiji <morinokumakamesan@gmail.com>
2018-04-15 19:06:08 +0900

    おすすめ画面の実装

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import HomePage from './pages/home/HomePage';
 import PostsPage from './pages/posts/PostsPage';
 import TestPage from './pages/test/Test';
+import Recommend from './pages/recommend/Recommend';
 
 export default [
   { name: 'home', path: '/', component: HomePage },
   { name: 'posts', path: '/posts', component: PostsPage },
   { name: 'test', path: '/test', component: TestPage },
+  { name: 'recommend', path: '/recommend', component: Recommend },
 ];
