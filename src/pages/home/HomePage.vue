347e0f46f0fd8315d641519db02b0a0edfc1297b
sotaro maehara <s.maehara81@icloud.com>
2018-04-15 22:32:58 +0900

    add new card

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -16,7 +16,6 @@
   .el-carousel__item:nth-child(2n) {
     background-color: #99a9bf;
   }
-
   .el-carousel__item:nth-child(2n+1) {
     background-color: #d3dce6;
   }
@@ -31,7 +30,7 @@
       <img src="../../assets/love.jpeg" width="12" height="12">
       デートプランを探す
     </v-ons-list-header>
-        <v-ons-list-item v-for='region in regions' :v-bind='region' @click="$router.push({ name: region.url});">
+        <v-ons-list-item v-for='region in regions' :v-bind='region.name' @click="$router.push({ name: region.url});">
           {{region.name}}
         </v-ons-list-item>
     </v-ons-list>
@@ -39,10 +38,9 @@
     <h3>話題のデート体験記</h3>
     <v-ons-carousel style="width: 100%; height: 200px"
       swipeable auto-scroll overscrollable
-      :index.sync="carouselIndex"
     >
       <v-ons-carousel-item v-for='item in experiences' 
-        :v-bind='item.title'
+        :v-bind='item'
         :style="{backgroundColor: item.color}"
         >
         <div style="tex-align: center; font-size: 30px; margin-top: 20px; margin-left: 10px; color: #fff;">
@@ -51,12 +49,22 @@
         <p style="margin-left: 30pt; color: #fff;">{{ item.detail }}</p>
       </v-ons-carousel-item>
     </v-ons-carousel>
-  
-    <div :style='dots'>
-      <span :index="dotIndex - 1" v-for="dotIndex in Object.keys(experiences).length" :key="dotIndex" style="cursor: pointer" @click="carouselIndex = dotIndex - 1">
-        {{ carouselIndex === dotIndex - 1 ? '\u25CF' : '\u25CB' }}
-      </span>
-    </div>
+    </v-ons-card>
+    <v-ons-card>
+    <h3>新着記事</h3>
+    <v-ons-carousel style="width: 100%; height: 200px"
+      swipeable auto-scroll overscrollable
+    >
+      <v-ons-carousel-item v-for='item in newcards' 
+        :v-bind='item'
+        :style="{backgroundColor: item.color}"
+        >
+        <div style="tex-align: center; font-size: 30px; margin-top: 20px; margin-left: 10px; color: #fff;">
+          {{ item.title }}
+        </div>
+        <p style="margin-left: 30pt; color: #fff;">{{ item.detail }}</p>
+      </v-ons-carousel-item>
+    </v-ons-carousel>
     </v-ons-card>
     <v-ons-button modifier="cta" style="margin: 6px 0" @click="$router.push({ name: 'create-plan' });">作成</v-ons-button>
   </div>
@@ -97,6 +105,20 @@ export default {
         },
         {
           title: 'ほげ',
+          detail: 'hogehoge',
+          path: 'detail-plan',
+          color: '#373B44',
+        },
+      ],
+      newcards: [
+        {
+          title: 'sawlowの遅漏体験',
+          detail: '...',
+          path: 'detail-plan',
+          color: '#085078',
+        },
+        {
+          title: 'ほげ2',
           detail: 'hogehoge',
           path: 'detail-plan',
           color: '#373B44',
