542565bab98ebd3855ef5f1c466d970a108d6d79
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2018-04-15 19:07:17 +0900

    デートプランの詳細ページを作成しました

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import HomePage from './pages/home/HomePage';
 import PostsPage from './pages/posts/PostsPage';
 import TestPage from './pages/test/Test';
+import DetailPlan from './pages/detail-plan/DetailPlan';
 
 export default [
   { name: 'home', path: '/', component: HomePage },
   { name: 'posts', path: '/posts', component: PostsPage },
   { name: 'test', path: '/test', component: TestPage },
+  { name: 'detail-plan', path: '/detail-plan', component: DetailPlan },
 ];
