bf6f33416a08739d12fc4f1f5ef46a18ebd065b7
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2018-04-17 23:10:47 +0900

    detailplan api add

--- src/pages/detail-plan/DetailPlan.vue
+++ src/pages/detail-plan/DetailPlan.vue
@@ -2,19 +2,19 @@
   <ons-page>
     <navbar navType="back" msg="プラン詳細"></navbar>    
     <v-ons-card>
-        <h2> プラン名：{{hello}}</h2>
+        <h2>{{post.title}}</h2>
         <h4 >概要:</h4>
-        <p>{{note}}</p>
+        <p>{{post.comment}}</p>
     </v-ons-card>
     <v-ons-card>
-        <h4>行ったスポット：{{spot}}</h4>
+        <h4>行ったスポット：</h4>
         <v-ons-list>
         <v-ons-list-item>Item A</v-ons-list-item>
         <v-ons-list-item>Item B</v-ons-list-item>
         </v-ons-list>
     </v-ons-card>
     <v-ons-card>
-        <h4>コース：{{cource}}</h4>
+        <h4>コース：</h4>
         <v-ons-list>
         <v-ons-list-item>Item A</v-ons-list-item>
         <v-ons-list-item>Item B</v-ons-list-item>
@@ -23,7 +23,7 @@
     <v-ons-card>
         <h4>感想：</h4>
         <p>めっちゃよかった</p>
-        <p>{{note}}</p>
+        <p></p>
     </v-ons-card>
     <v-ons-button modifier="cta" style="margin: 60px 0px" @click="$router.push({ name: 'recommend' });">戻る</v-ons-button>
     <v-ons-button modifier="cta" style="margin: 60px 0px" @click="$router.push({ name: 'home' });">ホームへ</v-ons-button>
@@ -31,6 +31,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import Navbar from '../../components/navbar/Navbar';
 import EhamaForm from '../../components/form/Form';
 
@@ -40,5 +41,34 @@ export default {
     Navbar,
     EhamaForm,
   },
+  data() {
+    return {
+      // url: 'http://59.157.6.140:3000/plans/1',
+      url: `http://59.157.6.140:3000/plans/${this.route.params.id}`,
+      post: [],
+    };
+  },
+  created() {
+    axios.get(this.url)
+    .then((response) => {
+      this.post = response.data;
+      console.log(response);
+    });
+  },
+
+
+  // data() {
+  //   return {
+  //     items: {
+  //       title: '稲佐山の夜景と君とforever',
+  //       overview: '1000万ドルの夜景とも揶揄される長崎市稲佐山の夜景は、意中の相手とのデートを一層盛り上げてくれるはず！！',
+  //       spots: ['レストランなんとか', '稲佐山', 'ラブホ'],
+  //       course: [
+  //         { time: '20:00', move: '夜景を見る' },
+  //         { time: '21:45', move: '飯食べる' },
+  //         { time: '23:00', move: 'xxx' }],
+  //     },
+  //   };
+  // },
 };
 </script>
