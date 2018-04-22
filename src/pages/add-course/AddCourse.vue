45edb9f0cf40f4fc610dc66d41c49bfc3b02527d
Tomohiro Matsuo <gacktomo@gmail.com>
2018-04-22 16:33:15 +0900

    googleMapApi追加

--- src/pages/add-course/AddCourse.vue
+++ src/pages/add-course/AddCourse.vue
@@ -18,7 +18,21 @@
 
       <v-ons-list-header>場所</v-ons-list-header>
       <v-ons-list-item>
-        <v-ons-input placeholder="スポット名を検索" float v-model="course.name"></v-ons-input>
+        <gmap-autocomplete
+          placeholder="スポット名を検索"
+          style="font-size:15px; margin: 6px 0; width:80%"
+          :value="course.name"
+          @place_changed="setPlace"
+          :options="{
+            bounds: {north: 45, south: 20, east: 153, west: 122},
+            strictBounds: true
+          }">
+        </gmap-autocomplete>
+        <div class="right">
+          <v-ons-button modifier="quiet" @click="resetPlace();$event.stopPropagation()">
+            <v-ons-icon icon="md-close"></v-ons-icon>
+          </v-ons-button>
+        </div>
       </v-ons-list-item>
 
       <v-ons-list-header>詳細</v-ons-list-header>
@@ -58,6 +72,7 @@ export default {
         name: '',
         description: '',
         time: "19:00",
+        place_id: '',
       },
       edit: false,
       idx: 0,
@@ -71,6 +86,7 @@ export default {
       this.edit = true;
       this.idx = this.$props.index;
     }
+
   },
   methods: {
     addCourse() {
@@ -80,6 +96,14 @@ export default {
         data: this.course,
       });
       this.$emit('pop-page');
+    },
+    setPlace(place) {
+      console.log(place);
+      this.course.name = place.name;
+      this.course.place_id = place.place_id;
+    },
+    resetPlace() {
+      this.course.name = '';
     },
   },
 };
